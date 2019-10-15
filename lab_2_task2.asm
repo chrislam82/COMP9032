612344a5d724cdb0e5c224d5b3507d2282b49b67
JohnSmith72 <chris.sc.lam29@gmail.com>
2019-10-15 13:16:09 +1100

    adding lab2_task2

--- lab_2_task2.asm
+++ lab_2_task2.asm
@@ -5,7 +5,7 @@
 ; Version: 1.0.0
 ; 
 ; File:
-;   LED on Arduino to display  a sequence of 3 patterns
+;   LED on Arduino to display a sequence of 3 patterns
 ;   with 0.5sec delay stored in registers before execution.
 ;   Display halts when a button is pressed. Clock frequency of 16MHz
 ;
